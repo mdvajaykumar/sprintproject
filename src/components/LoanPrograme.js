490421471b6b2de8436303548fd56b6fbef2bc4a
ajaykumar <majaykumar481@gmail.com>
2021-06-06 09:39:33 +0530

    up

--- src/components/LoanPrograme.js
+++ src/components/LoanPrograme.js
@@ -2,16 +2,28 @@ export function LoanPrograme() {
   return (
     <div>
       <div className="text-center">Loan Programme Page</div>
-      <div>
-        <div clasName="first-part">
+      <div className="first-part">
+        <div>
           <h1>home Loan </h1>
           <p> take the loan at minimum price and enjoy the benefits</p>
           <button className="btn btn-primary">apply</button>
-          <div>
-            <h1>home Loan </h1>
-            <p> take the loan at minimum price and enjoy the benefits</p>
-            <button className="btn btn-primary">apply</button>
-          </div>
+        </div>
+        <div>
+          <h1>car Loan</h1>
+          <p> take the loan at minimum price and enjoy the benefits</p>
+          <button className="btn btn-primary">apply</button>
+        </div>
+      </div>
+      <div className="first-part">
+        <div>
+          <h1>BusinessLoan</h1>
+          <p> take the loan at minimum price and enjoy the benefits</p>
+          <button className="btn btn-primary">apply</button>
+        </div>
+        <div>
+          <h1>Education Loan</h1>
+          <p> take the loan at minimum price and enjoy the benefits</p>
+          <button className="btn btn-primary">apply</button>
         </div>
       </div>
     </div>
